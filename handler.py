7076ce4842e53eb8ef1ea4b3e8dcb75fbf6161fa
ErikBoesen <me@erikboesen.com>
2023-09-19 13:55:22 -0400

    Add handler

---
+++ handler.py
@@ -0,0 +1,66 @@
+import sys
+sys.path.insert(0, 'vendor')
+
+import os
+import requests
+import random
+import json
+
+
+API_ROOT = 'https://api.groupme.com/v3/'
+FLAGGED_PHRASES = (
+    'essay written by professionals',
+    'paper writing service',
+    'academic writing service',
+    'student paper assignments',
+    'cutt.us',
+    'inyurl.com/muxz7h',
+)
+
+
+def get_memberships(group_id, token):
+    response = requests.get(f'{API_ROOT}groups/{group_id}', params={'token': token}).json()['response']['members']
+    print('Got response: ' + str(response))
+    return response
+
+
+def get_membership_id(group_id, user_id, token):
+    memberships = get_memberships(group_id, token)
+    for membership in memberships:
+        if membership['user_id'] == user_id:
+            return membership['id']
+
+
+def remove_member(group_id, membership_id, token):
+    response = requests.post(f'{API_ROOT}groups/{group_id}/members/{membership_id}/remove', params={'token': token})
+    return response.ok
+
+
+def kick_user(group_id, user_id, token):
+    membership_id = get_membership_id(group_id, user_id, token)
+    remove_member(group_id, membership_id, token)
+
+
+def receive(event, context):
+    message = json.loads(event['body'])
+
+    bot_id = message['bot_id']
+    for phrase in FLAGGED_PHRASES:
+        if phrase in message['text'].lower():
+            kick_user(message['group_id'], message['user_id'], message['token'])
+            send('Kicked ' + message['name'] + ' due to apparent spam post.', bot_id)
+
+    return {
+        'statusCode': 200,
+        'body': 'ok'
+    }
+
+
+def send(text, bot_id):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    message = {
+        'bot_id': bot_id,
+        'text': text,
+    }
+    r = requests.post(url, json=message)
